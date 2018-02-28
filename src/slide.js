a7ca65810e5f23bfd24853f5ca4c950dc478c237
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-02-28 18:01:22 +0530

    remove lazyloading images.

--- src/slide.js
+++ src/slide.js
@@ -1,42 +1,9 @@
-const imageIsLoaded = url =>
-
-  new Promise(function (resolve, reject) {
-
-    const img = new Image();
-
-    img.addEventListener('load', function () {
-
-      resolve(true);
-
-    }, false);
-
-    img.addEventListener('error', function () {
-
-      reject(new Error(
-        'Could not get the image for this slide.' +
-        'Using image in preview.'
-      ));
-
-    }, false);
-
-    img.src = url;
-
-  });
-
 export const setSlideURLandTitle = globals => {
 
-  const previewUrl = globals.config.slides[globals.slideIndex].preview;
   const slideUrl = globals.config.slides[globals.slideIndex].content;
   const slideCaption = globals.config.slides[globals.slideIndex].caption;
 
-  globals.vsbStage.style.backgroundImage = `url(${previewUrl}`;
-
-  imageIsLoaded(slideUrl).then(() => {
-
-    globals.vsbStage.style.backgroundImage = `url(${slideUrl}`;
-
-  }).catch(value => console.error(value)); // eslint-disable-line no-console
-
+  globals.vsbStage.style.backgroundImage = `url(${slideUrl}`;
   globals.vsbCaption.innerText = slideCaption;
 
 };
